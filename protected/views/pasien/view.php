c876ed10f3a197d8ef6c18b37e6a777293f2476e
Sumardi <sumardi@rekadia.co.id>
2023-08-19 08:27:27 +0700

    Fix pasien

--- protected/views/pasien/view.php
+++ protected/views/pasien/view.php
@@ -1,20 +1,3 @@
-<?php
-/* @var $this PasienController */
-/* @var $model Pasien */
-
-$this->breadcrumbs=array(
-	'Pasiens'=>array('index'),
-	$model->id,
-);
-
-$this->menu=array(
-	array('label'=>'List Pasien', 'url'=>array('index')),
-	array('label'=>'Create Pasien', 'url'=>array('create')),
-	array('label'=>'Update Pasien', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Pasien', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Pasien', 'url'=>array('admin')),
-);
-?>
 
 <h1>View Pasien #<?php echo $model->id; ?></h1>
 
@@ -22,10 +5,22 @@ $this->menu=array(
 	'data'=>$model,
 	'attributes'=>array(
 		'id',
-		'idTindakan',
-		'idObat',
-		'idPegawai',
-		'idWilayah',
+		array(
+			'name'=>'idTindakan0.namaTindakan',
+			'label'=>'Tindakan',
+		),
+		array(
+			'name'=>'idObat0.name',
+			'label'=>'Obat',
+		),
+		array(
+			'name'=>'idPegawai0.name',
+			'label'=>'Pegawai',
+		),
+		array(
+			'name'=>'idWilayah0.name',
+			'label'=>'Wilayah',
+		),
 		'address',
 		'jenisKelamin',
 	),
