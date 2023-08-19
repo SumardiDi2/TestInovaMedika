c38ad015ab33e81585ca44994d6135eccdf1a886
Sumardi <sumardi@rekadia.co.id>
2023-08-19 16:27:47 +0700

    remove comment code

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -46,22 +46,6 @@ class UserIdentity extends CUserIdentity
 			$this->errorCode=self::ERROR_NONE;
 		}
 
-		// if(!isset($users[$this->username]))
-		// 	$this->errorCode=self::ERROR_USERNAME_INVALID;
-		// elseif($users[$this->username]!==$this->password)
-		// 	$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		// else
-		// 	$this->errorCode=self::ERROR_NONE;
-
         return !$this->errorCode;
-
-		
-		// array_push($users, "user","123");
-
-		// foreach ($record as $value) {
-		// 	array_push($users, "user","123");
-		// }
-		
-		// return !$this->errorCode;
 	}
 }
